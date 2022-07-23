6de032d74ce8f7051186fe9facd8a58bb81b098f
wywxz <53309090+wywxz@users.noreply.github.com>
2022-07-23 14:45:33 -0400

    update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -120,8 +120,8 @@ async def private_receive_handler(c: Client, m: Message):
             parse_mode="HTML", 
             quote=True,
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🖥STREAM", url=stream_link), #Stream Link
-                                                InlineKeyboardButton('Dᴏᴡɴʟᴏᴀᴅ📥', url=online_link)]]) #Download Link
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🖥 STREAM", url=stream_link), #Stream Link
+                                                InlineKeyboardButton("DOWNLOAD 📥", url=online_link)]]) #Download Link
         )
     except FloodWait as e:
         print(f"Sleeping for {str(e.x)}s")
@@ -157,8 +157,8 @@ async def channel_receive_handler(bot, broadcast):
             message_id=broadcast.message_id,
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton("🖥 Stream", url=stream_link),
-                     InlineKeyboardButton("Download 📥", url=online_link)] 
+                    [InlineKeyboardButton("🖥 STREAM", url=stream_link),
+                     InlineKeyboardButton("DOWNLOAD 📥", url=online_link)] 
                 ]
             )
         )
